06cc5612c3c946e85f1c64abdeefc2185ce0d792
NorthPoleDemo <LyndonW@npe-inc.com>
2017-10-25 09:10:10 -0500

    Put ANT radio into controlled mode
    Demonstrates how to put an ANT radio into controlled mode in order to send Fitness Equipment Control command.
    This example sets the Basic Resistance of a Trainer.

---
+++ ControlAntRadio/MainDialog.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ControlAntRadio
+{
+    partial class MainDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.checkBoxUsb = new System.Windows.Forms.CheckBox();
+            this.listViewSensors = new System.Windows.Forms.ListView();
+            this.columnHeaderType = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Dock = System.Windows.Forms.DockStyle.Left;
+            this.buttonStart.Location = new System.Drawing.Point(0, 0);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 30);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // checkBoxUsb
+            // 
+            this.checkBoxUsb.AutoSize = true;
+            this.checkBoxUsb.Dock = System.Windows.Forms.DockStyle.Right;
+            this.checkBoxUsb.Location = new System.Drawing.Point(265, 0);
+            this.checkBoxUsb.Name = "checkBoxUsb";
+            this.checkBoxUsb.Size = new System.Drawing.Size(48, 30);
+            this.checkBoxUsb.TabIndex = 2;
+            this.checkBoxUsb.Text = "USB";
+            this.checkBoxUsb.UseVisualStyleBackColor = true;
+            // 
+            // listViewSensors
+            // 
+            this.listViewSensors.AutoArrange = false;
+            this.listViewSensors.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderType,
+            this.columnHeaderNumber});
+            this.listViewSensors.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewSensors.GridLines = true;
+            this.listViewSensors.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listViewSensors.Location = new System.Drawing.Point(0, 0);
+            this.listViewSensors.Name = "listViewSensors";
+            this.listViewSensors.Size = new System.Drawing.Size(313, 466);
+            this.listViewSensors.Sorting = System.Windows.Forms.SortOrder.Descending;
+            this.listViewSensors.TabIndex = 3;
+            this.listViewSensors.UseCompatibleStateImageBehavior = false;
+            this.listViewSensors.View = System.Windows.Forms.View.Details;
+            this.listViewSensors.DoubleClick += new System.EventHandler(this.listViewSensors_DoubleClick);
+            // 
+            // columnHeaderType
+            // 
+            this.columnHeaderType.Text = "Device";
+            this.columnHeaderType.Width = 172;
+            // 
+            // columnHeaderNumber
+            // 
+            this.columnHeaderNumber.Text = "ID";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonStart);
+            this.panel1.Controls.Add(this.checkBoxUsb);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 436);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(313, 30);
+            this.panel1.TabIndex = 4;
+            // 
+            // MainDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 466);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listViewSensors);
+            this.Name = "MainDialog";
+            this.Text = "ANT Radio Control Example";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.CheckBox checkBoxUsb;
+        private System.Windows.Forms.ListView listViewSensors;
+        private System.Windows.Forms.ColumnHeader columnHeaderType;
+        private System.Windows.Forms.ColumnHeader columnHeaderNumber;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ ControlAntRadio/MainDialog.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NorthPoleEngineering.WaspClassLibrary;
+using NorthPoleEngineering.WaspClassLibrary.FitnessEquipmentControls;
+
+namespace ControlAntRadio
+{
+    public partial class MainDialog : Form
+    {
+        WaspCollection _wasps;
+        AntPlusDevices _sensors;
+        FitnessEquipment _fitnessEquipment;
+
+        public MainDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (checkBoxUsb.Checked)
+            {
+                _wasps = new WaspCollection(Handle);
+            }
+            else
+            {
+                _wasps = new WaspCollection();
+            }
+            _wasps.CollectionChanged += Wasps_CollectionChanged;
+            // connect to ANT device notifications
+            _sensors = new AntPlusDevices(_wasps);
+            _sensors.AntPlusDeviceEvent += NewSensors_AntPlusDeviceEvent; ;
+
+            buttonStart.Enabled = false;
+        }
+
+        private void NewSensors_AntPlusDeviceEvent(object sender, AntPlusDeviceEventArgs e)
+        {
+            ListViewItem item = new ListViewItem(e.DeviceType.ToString());
+            item.SubItems.Add(e.ExtendedDeviceNumber.ToString());
+            //item.SubItems.Add(e.DeviceNumber.ToString());
+            //item.SubItems.Add(e.Device.RSSI.ToString());
+            item.Tag = e.Device;
+            listViewSensors.Items.Add(item);
+        }
+
+        private void Wasps_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+           /* if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                foreach (object o in e.NewItems)
+                {
+                    listBox1.Items.Add((Wasp)o);
+                }
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                foreach (object o in e.NewItems)
+                {
+                    listBox1.Items.Remove((Wasp)o);
+                }
+            }*/
+        }
+
+        /// <summary>
+        /// Called when the connection completes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Wasp_ConnectionEvent(object sender, ConnectionEventArgs e)
+        {
+            if (e.Connection == NorthPoleEngineering.WaspClassLibrary.ConnectionState.Connected)
+            {
+                Wasp wasp = sender as Wasp;
+
+                // check if not WASP-B
+                if (wasp.ProductType != ProductTypes.WASP_B)
+                {
+                    // create a list of ANT radios we want to control
+                    List<object> radios = new List<object>();
+                    RadioTypes[] radioTypes = { wasp.Radio1Type, wasp.Radio2Type, wasp.Radio3Type, wasp.Radio4Type };
+
+                    for (int i = 0; i < radioTypes.Length; i++)
+                    {
+                        if (radioTypes[i] == RadioTypes.ANT)
+                        {
+                            // order is radio index, radio mode
+                            radios.AddRange(new object[] { i, Wasp.AntRadioModes.Controlled });
+
+                            // connect to the radio reset event
+                            AntRadio antRadio = wasp.GetAntRadio(i);
+                            antRadio.ResetEvent += Radio_ResetEvent; ;
+                            antRadio.Retries = 0;
+                        }
+                    }
+
+                    if (radios.Count > 0)
+                    {
+                        // this will reset the radios
+                        wasp.SetAntRadioMode(radios.ToArray());
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Called when a radio is reset
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Radio_ResetEvent(object sender, EventArgs e)
+        {
+            AntRadio antRadio = sender as AntRadio;
+
+            // disconnect from the reset event
+            antRadio.ResetEvent -= Radio_ResetEvent;
+
+            // the status event will occur when radio initialization is complete
+            antRadio.StatusEvent += AntRadio_StatusEvent;
+
+            // initialze the ANT radio for continuous scanning mode on channel 57
+            antRadio.ConfigureContinuousScanningMode(AntDeviceType.UnknownSensor, 57);
+        }
+
+        private void AntRadio_StatusEvent(object sender, AntRadio.ChannelStatus e)
+        {
+            AntRadio antRadio = sender as AntRadio;
+            // disconnect from the status event
+            antRadio.StatusEvent -= AntRadio_StatusEvent;
+
+            if (_fitnessEquipment != null)
+            {
+                SetBasicResistance();
+            }
+        }
+
+        /// <summary>
+        /// Called when a sensor is clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listViewSensors_DoubleClick(object sender, EventArgs e)
+        {
+            AntDevice antDevice = (AntDevice)listViewSensors.SelectedItems[0].Tag;
+            _fitnessEquipment = antDevice as FitnessEquipment;
+
+            Wasp wasp = antDevice.GetStrongestWasp();
+            wasp.ConnectionEvent += Wasp_ConnectionEvent; ;
+            wasp.RequestConnection();
+        }
+
+        /// <summary>
+        /// Sets the resistance of a Fitness Equipment Trainer
+        /// </summary>
+        private void SetBasicResistance()
+        {
+            if (_fitnessEquipment != null)
+            {
+                _fitnessEquipment.SetBasicResistance(10);
+            }
+        }
+    }
+}

7becd14c9ed892ba0f39f62b0ef66254df48cc4e
NorthPoleDemo <LyndonW@npe-inc.com>
2018-03-01 12:37:32 -0600

    Example of reading data from multiple WASPs

---
+++ BasicWasp/BasicWasp/DeviceFormatter.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using NorthPoleEngineering.WaspClassLibrary;
+using NorthPoleEngineering.WaspClassLibrary.FitnessEquipmentControls;
+
+namespace BasicWasp
+{
+    /// <summary>
+    /// This class receives a specific ANT object and formats it for display
+    /// </summary>
+    class DeviceFormatter
+    {
+        /// <summary>
+        ///  List of known ANT/BLE devices
+        /// </summary>
+        readonly List<string> _knownDevices = new List<string> {
+        "BikeCadenceSensor",
+        "BikePowerSensor",
+        "BikeSpeedAndCadenceSensor",
+        "BikeSpeedSensor",
+        "EnvironmentSensor",
+        "FitnessEquipment",
+        "Geocache",
+        "HeartRateMonitor",
+        "PairingPod",
+        "PolarH7HeartRateMonitors",
+        "SuuntoHeartRateMonitors",
+        "SdmPod",
+        "UnknownDevice",
+        "WFKickr",
+        "MuscleOxygenMonitor"
+        };
+
+        /// <summary>
+        /// Returns a dictionary of data from the device passed in
+        /// </summary>
+        /// <param name="device">Device sending data</param>
+        /// <param name="args">arguments provided</param>
+        /// <returns></returns>
+        public static List<string> FormatDevice(AntDevice device, EventArgs args)
+        {
+            try
+            {
+                object[] parameters = { device, args };
+                return (List<string>)typeof(DeviceFormatter).InvokeMember(
+                    "Format" + device.DeviceType.ToString(),
+                    BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Static,
+                    null,
+                    null,
+                    parameters);
+            }
+            catch(MissingMethodException )
+            {
+                return new List<string> { "Unknown device" ,  "Type: "+ device.DeviceType.ToString() };
+            }
+        }
+
+        /// <summary>
+        /// Returns a list of user-pertinent data from the heart rate monitor
+        /// </summary>
+        /// <param name="h">HeartRateMonitor device</param>
+        /// <param name="a">Device arguments</param>
+        /// <returns></returns>
+        public static List<string> FormatHeartRateMonitor(object h, object a)
+        {
+            HeartRateMonitor hrm = h as HeartRateMonitor;
+            List<string> data = new List<string>();
+            data.Add("ID: " + hrm.ExtendedDeviceNumber.ToString());
+            data.Add("Type: " + hrm.DeviceType.ToString());
+            if (hrm.Manufacturer != null)
+            {
+                data.Add(string.Format("Mfr: {0}", hrm.Manufacturer.ManufacturerName));
+                data.Add(string.Format("Model: {0}", hrm.Manufacturer.ModelNumber));
+            }
+
+            HeartRateMonitorMessageEventArgs args = a as HeartRateMonitorMessageEventArgs;
+            if (args != null)
+            {
+                data.Add("Heartrate: "+ args.FormattedHeartRate(true));
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Returns a list of user-pertinent data from the BikeSpeedSensor
+        /// </summary>
+        /// <param name="h">BikeSpeedSensor device</param>
+        /// <param name="a">Device arguments</param>
+        /// <returns></returns>
+        public static List<string> FormatBikeSpeedSensor(object h, object a)
+        {
+            BikeSpeedSensor hrm = h as BikeSpeedSensor;
+            List<string> data = new List<string>();
+            data.Add("ID: " + hrm.ExtendedDeviceNumber.ToString());
+            data.Add("Type: " + hrm.DeviceType.ToString());
+            if (hrm.Manufacturer != null)
+            {
+                data.Add(string.Format("Mfr: {0}", hrm.Manufacturer.ManufacturerName));
+                data.Add(string.Format("Model: {0}", hrm.Manufacturer.ModelNumber));
+            }
+
+            BikeSpeedMessageEventArgs args = a as BikeSpeedMessageEventArgs;
+            if (args != null)
+            {
+                data.Add(string.Format("Speed: {0:F1}", args.InstantaneousSpeed));
+                data.Add("Revs: " + args.AccumulatedRevolutionCount.ToString());
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Returns a list of user-pertinent data from the BikeSpeedSensor
+        /// </summary>
+        /// <param name="h">BikeSpeedSensor device</param>
+        /// <param name="a">Device arguments</param>
+        /// <returns></returns>
+        public static List<string> FormatBikePowerSensor(object h, object a)
+        {
+            BikePowerSensor bp = h as BikePowerSensor;
+            List<string> data = new List<string>();
+            data.Add("ID: " + bp.ExtendedDeviceNumber.ToString());
+            data.Add("Type: " + bp.DeviceType.ToString());
+            if (bp.Manufacturer != null)
+            {
+                data.Add(string.Format("Mfr: {0}", bp.Manufacturer.ManufacturerName));
+                data.Add(string.Format("Model: {0}", bp.Manufacturer.ModelNumber));
+            }
+
+            PowerOnlyEventArgs args = a as PowerOnlyEventArgs;
+            if (args != null)
+            {
+                data.Add(string.Format("Cadence: {0:F1}", args.InstantaneousCadence));
+                data.Add(string.Format("Avg Power: {0:F1}", args.AveragePower));
+                data.Add(string.Format("Pedal Power: {0:F1}", args.PedalPower));
+                data.Add(string.Format("Accum Power: {0:F1}", args.AccumulatedPower));
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Returns a list of user-pertinent data from the BikeSpeedSensor
+        /// </summary>
+        /// <param name="h">BikeSpeedSensor device</param>
+        /// <param name="a">Device arguments</param>
+        /// <returns></returns>
+        public static List<string> FormatMuscleOxygenMonitor(object h, object a)
+        {
+            MuscleOxygenMonitor moxy = h as MuscleOxygenMonitor;
+            List<string> data = new List<string>();
+            data.Add("ID: " + moxy.ExtendedDeviceNumber.ToString());
+            data.Add("Type: " + moxy.DeviceType.ToString());
+            if (moxy.Manufacturer != null)
+            {
+                data.Add(string.Format("Mfr: {0}", moxy.Manufacturer.ManufacturerName));
+                data.Add(string.Format("Model: {0}", moxy.Manufacturer.ModelNumber));
+            }
+
+            data.Add(string.Format("THb: {0}", moxy.TotalHemoglobinConcentration));
+            data.Add(string.Format("Current SMO2: {0}%", moxy.CurrentSaturatedHemoglobin));
+            data.Add(string.Format("Previous SMO2: {0}%", moxy.PreviousSaturatedHemoglobin));
+            string tooHigh = moxy.AmbientLightTooHigh ? "too high" : "OK";
+            data.Add(string.Format("Ambient light: {0}", tooHigh));
+            if (moxy.Battery != null) 
+            {
+                data.Add(string.Format("Battery: {0:0.0}V", moxy.Battery.Voltage));
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Returns a list of user-pertinent data from the FitnessEquipment
+        /// </summary>
+        /// <param name="h">FitnessEquipment device</param>
+        /// <param name="a">Device arguments</param>
+        /// <returns></returns>
+        public static List<string> FormatFitnessEquipment(object h, object a)
+        {
+            FitnessEquipment fe = h as FitnessEquipment;
+            List<string> data = new List<string>();
+            data.Add("ID: " + fe.ExtendedDeviceNumber.ToString());
+            data.Add("Type: " + fe.DeviceType.ToString());
+            if (fe.Manufacturer != null)
+            {
+                data.Add(string.Format("Mfr: {0}", fe.Manufacturer.ManufacturerName));
+                data.Add(string.Format("Model: {0}", fe.Manufacturer.ModelNumber));
+            }
+
+            TrainerMessageEventArgs tr = a as TrainerMessageEventArgs;
+            if (tr != null)
+            {
+                data.Add(string.Format("Cadence: {0}", tr.InstantaneousCadence));
+                data.Add(string.Format("Power: {0}", tr.AccumulatedPower));
+            }
+            else
+            {
+                RowerMessageEventArgs r = a as RowerMessageEventArgs;
+                if (r != null)
+                {
+                    data.Add(string.Format("Cadence: {0}", r.InstantaneousCadence));
+                    data.Add(string.Format("Strokes: {0}", r.AccumulatedStrokeCount));
+                }
+            }
+            return data;
+        }
+
+        /// <summary>
+        /// Display data for unknown ANT sensor types
+        /// </summary>
+        /// <param name="j"></param>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static List<string> FormatUnknownSensor(object j, object a)
+        {
+            return new List<string> { "Unknown device", "Type: Unknown"};
+        }
+
+    }
+}
---
+++ BasicWasp/BasicWasp/MainWindow.xaml.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+
+using NorthPoleEngineering.WaspClassLibrary;
+using NorthPoleEngineering.WaspClassLibrary.FitnessEquipmentControls;
+
+namespace BasicWasp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Create a class to hold displayable WASP data
+        /// </summary>
+        public class WaspData
+        {
+            public string MAC { get; set; }
+            public string FW { get; set; }
+            public string Type { get; set; }
+            public string Name { get; set; }
+            public string ChargingState { get; set; }
+            public string Voltage { get; set; }
+            public string FuelGauge { get; set; }
+
+            public WaspData()
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Build a collection of WASPs we get data from
+        /// </summary>
+        public ObservableCollection<WaspData> _waspData;
+
+        WaspCollection _wasps;
+        AntPlusDevices _ants;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //OEMCryptoClass.AddKeys(PrivateKeychain.Keychain.Keys);
+            _waspData = new ObservableCollection<WaspData>();
+            DataGridWasps.DataContext = _waspData;
+        }
+
+        /// <summary>
+        /// Creates a WASP collection  which will then accept data from BLE and ANT devices
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Create a collection of wasps on all available networks
+            if (checkBoxUsb.IsChecked == true)
+            {
+                WindowInteropHelper w = new WindowInteropHelper(this);
+                _wasps = new WaspCollection(WaspCollection.DefaultMulticastPort, w.Handle);
+            }
+            else
+            {
+                _wasps = new WaspCollection();
+            }
+
+            _wasps.CollectionVerbosity = Wasp.WaspLogLevel.All;
+            // Get Query response message
+            _wasps.CollectionChanged += wasps_CollectionChanged;
+            // Extract the list of ANT devices reported by the WASPs
+            _ants = new AntPlusDevices(_wasps);
+            _ants.AntPlusDeviceEvent += _ants_AntPlusDeviceEvent;
+            _ants.KeiserM3Bikes.BluetoothNewDeviceEvent += KeiserM3Bikes_BluetoothNewDeviceEvent; 
+            StartButton.IsEnabled = false;
+        }
+
+        /// <summary>
+        /// Display WASP information
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void wasps_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+            {
+                Wasp w = e.NewItems[0] as Wasp;
+                try
+                {
+                    WaspData wd = new WaspData
+                    {
+                        ChargingState = w.IsCharging.ToString(),
+                        FW = string.Format("{0}.{1}", w.VersionMajor, w.VersionMinor),
+                        MAC = BitConverter.ToString(w.MAC),
+                        Name = w.Name,
+                        Type = TypeDescriptor.GetConverter(w.ProductType).ConvertToString(w.ProductType),
+                        Voltage = string.Format("{0:0.00}", w.BatteryLevel),
+                        FuelGauge = string.Format("{0:0.00}", w.FuelGauge)
+                    };
+                    _waspData.Add(wd);
+                }
+                catch (ArgumentNullException)
+                {
+                    // USB mode does not have MAC address at this time
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handler for Keiser M3 bluetooth bike
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void KeiserM3Bikes_BluetoothNewDeviceEvent(object sender, KeiserM3EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// This method is called when an ANT device is added or removed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void _ants_AntPlusDeviceEvent(object sender, AntPlusDeviceEventArgs e)
+        {
+            // Format data and display
+            GroupBox g = CreateDeviceBox(e.Device, e);                   
+            if (g != null)
+            {
+                MainPanel.Children.Add(g);
+
+                // Register heart rate monitor for data updates
+                if (e.DeviceType == AntDeviceType.HeartRateMonitor)
+                {
+                    ((HeartRateMonitor)(e.Device)).HeartRateMessageEvent += MainWindow_AntMessageEvent;
+                }
+                else if (e.DeviceType == AntDeviceType.BikeSpeedSensor)
+                {
+                    ((BikeSpeedSensor)(e.Device)).BikeSpeedMessageEvent += MainWindow_AntMessageEvent;
+                }
+                else if (e.DeviceType == AntDeviceType.BikePowerSensor)
+                {
+                    ((BikePowerSensor)(e.Device)).BikePowerOnlyEvent += MainWindow_AntMessageEvent;
+                }
+                else if (e.DeviceType == AntDeviceType.MuscleOxygenMonitor)
+                {
+                    ((MuscleOxygenMonitor)(e.Device)).PropertyChanged += MainWindow_AntMessageEvent;
+                }
+                else if (e.DeviceType == AntDeviceType.FitnessEquipment)
+                {
+                    ((FitnessEquipment)(e.Device)).FitnessEquipmentEvent += MainWindow_AntMessageEvent;
+                }
+                else if (e.DeviceType == AntDeviceType.UnknownSensor)
+                {
+                    ((UnknownDevice)(e.Device)).PropertyChanged += MainWindow_AntMessageEvent;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Generic handler for ANT message updates
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MainWindow_AntMessageEvent(object sender, EventArgs e)
+        {
+            // Find the listview it corresponds to and refresh            
+            string targetName = "ID: " + ((AntDevice)sender).ExtendedDeviceNumber.ToString();
+            try
+            {
+                GroupBox gb = MainPanel.Children.OfType<GroupBox>().First<GroupBox>(x => x.Header.ToString() == targetName);
+                ListView lv = gb.Content as ListView;
+
+                List<string> newData = DeviceFormatter.FormatDevice((AntDevice)sender, e);
+                lv.ItemsSource = null;
+                lv.ItemsSource = newData;
+                lv.Items.Refresh();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Format a group box for the device
+        /// </summary>
+        /// <param name="dev"></param>
+        /// <param name="args">Arguments, if any</param>
+        /// <returns></returns>
+        GroupBox CreateDeviceBox(AntDevice dev, EventArgs args)
+        {
+            GroupBox gb = null;
+            List<string> d = DeviceFormatter.FormatDevice(dev, args);
+            gb = new GroupBox();
+            // First item in the list is the ID
+            gb.Header = d[0];
+            ListView lv = new ListView();
+            lv.ItemsSource = d;
+            lv.Margin = new Thickness(5);
+            lv.Padding = new Thickness(5);
+            gb.Content = lv;
+            return gb;
+        }
+    }
+}
